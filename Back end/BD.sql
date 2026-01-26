082e8805fc2d05e3b027b1d70d87e0097a773818
Gab-Zag <gab.alves.pereira30@gmail.com>
2026-01-26 04:15:01 -0300

    feat: Add initial database schema and update README with project details

---
+++ Back end/BD.sql
@@ -0,0 +1,40 @@
+Create TABLE if NOT EXISTS client(
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50),
+	email VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL
+);
+
+-- CREATE TYPE tipo_categoria AS ENUM ('Receita', 'Despesa');
+
+CREATE TABLE if NOT EXISTS category(
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50),
+	type tipo_categoria,
+	client_id int NOT NULL,
+
+	CONSTRAINT fk_category_client
+		FOREIGN KEY (client_id)
+		REFERENCES client (id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE if not EXISTS transaction(
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	description VARCHAR(100),
+	value DECIMAL(12,2),
+	type tipo_categoria,
+	transaction_date TIMESTAMP,
+	category_id int NOT NULL,
+	client_id int NOT NULL,
+
+	CONSTRAINT fk_transaction_client
+		FOREIGN KEY (client_id)
+		REFERENCES client (id)
+		ON DELETE CASCADE,
+
+	CONSTRAINT fk_transaction_category
+		FOREIGN KEY (category_id)
+		REFERENCES category (id)
+		ON DELETE CASCADE
+);
--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # Projeto-Sistema-de-Controle-Financeiro-Pessoal
+O objetivo do projeto é praticar a criação de um CRUD completo.
+# Funcionalidades (CRUD)
+## Usuário
+Cadastro
+Login
+Editar perfil
+Excluir conta
+# Transações
+Criar transação (receita ou despesa)
+
+Listar transações
+
+Editar transação
+
+Excluir transação
+
+## Campos:
+
+Descrição
+
+Valor
+
+Tipo (RECEITA | DESPESA)
+
+Data
+
+Categoria
+
+# Categorias
+Criar categoria
+
+Listar categorias
+
+Editar categoria
+
+Excluir categoria
+
+## Exemplos:
+
+Alimentação
+
+Aluguel
+
+Transporte
+
+Lazer
+
+Salário
+
+# Banco de Dados
+
+## Usuario
+```
+id 
+nome
+email
+senha
+```
+
+## Cadegoria
+```
+id
+nome
+tipo (RECEITA | DESPESA)
+usuario_id (fk)
+```
+
+## Transação
+```
+id
+descrição
+valor
+tipo (Receita | Despesa)
+data
+categoria_id (FK)
+usuario_id (FK)
+```
+
+## relacionamentos:
+
+Usuário -> Categorias
+
+Usuário -> Transação
+
+Categoria -> Transação
